ceafd1207d441f698a4121315dcfcbd1fb5855db
Amina Cherni <cherniamina21@gmail.com>
2023-05-11 01:06:26 +0200

    affichage des contacts

--- src/main/java/com/example/tpcarnet/TpCarnetApplication.java
+++ src/main/java/com/example/tpcarnet/TpCarnetApplication.java
@@ -28,6 +28,7 @@ public class TpCarnetApplication implements CommandLineRunner {
         Contact c2 = new Contact("firstname2", "lastname2");
         user1.getContactList().add(c2);
         c1.setUser(user1);
+        c2.setUser(user1);
         userRepository.save(user1);
 
         User user2 = new User("JSDGhon", "doe");
@@ -35,7 +36,8 @@ public class TpCarnetApplication implements CommandLineRunner {
         user2.getContactList().add(c3);
         Contact c4 = new Contact("firstname4", "lastname4");
         user2.getContactList().add(c4);
-        c2.setUser(user2);
+        c3.setUser(user2);
+        c4.setUser(user2);
         userRepository.save(user2);
 
     }
